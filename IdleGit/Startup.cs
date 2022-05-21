00c85feca833b6a40c73fd312911740cf1c697d1
Allen Proulx <Allen.Proulx232@gmail.com>
2022-05-21 13:48:16 -0400

    Add startup

---
+++ IdleGit/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdleGit;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace IdleGit
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = InitializeConfiguration(builder.Services.BuildServiceProvider());
+        }
+
+        protected virtual IConfiguration InitializeConfiguration(IServiceProvider provider)
+        {
+            var config = provider.GetService<IConfiguration>();
+
+            config = new ConfigurationBuilder()
+                .AddConfiguration(config)
+                .Build();
+
+            return config;
+        }
+    }
+}
