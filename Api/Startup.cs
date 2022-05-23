80500e720c9d1b9b55185facd1bc6a939186a1b6
Allen Proulx <Allen.Proulx232@gmail.com>
2022-05-22 22:17:03 -0400

    Add swagger

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Application;
+using Microsoft.OpenApi.Models;
 using Octokit.Webhooks;
 using Octokit.Webhooks.AspNetCore;
 
@@ -6,11 +7,20 @@ namespace Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        private const string ApiTitle = "Idle Git API";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddSingleton<WebhookEventProcessor, MyWebhookEventProcessor>();
             services.AddSingleton<IScoreProvider, ScoreProvider>();
+            ConfigureSwagger(services);
         }
 
         public void Configure(IApplicationBuilder app)
@@ -21,6 +31,30 @@ namespace Api
             {
                 endpoints.MapGitHubWebhooks();
                 endpoints.MapControllers();
+            });
+
+            UseSwagger(app);
+        }
+
+        private static void UseSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", ApiTitle);
+            });
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = ApiTitle,
+                    Version = "v1"
+                });
             });
         }
     }
